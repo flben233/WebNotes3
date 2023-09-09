53743491e6775345c9808a4fe6b3eace130efb3b
ShirakawaTyu <1400070301@qq.com>
2023-09-09 23:09:29 +0800

    - 修复了图片不能正常加载的问题

--- src/common/utils.js
+++ src/common/utils.js
@@ -3,10 +3,14 @@ import {ROOT_DOMAIN} from "@/common/final";
 export function setImgElement() {
     let elements = document.getElementsByTagName("img");
     let cookies = document.cookie.split(";");
+    let saToken = null;
     for (let cookie of cookies) {
         if (cookie.includes("satoken")) {
-            let saToken = cookie.split("=");
-            document.cookie = "satoken=" + saToken[1] + ";domain=." + ROOT_DOMAIN + ";max-age=1";
+            saToken = cookie.split("=");
+            document.cookie = "satoken=" + saToken[1] + ";domain=." + ROOT_DOMAIN + ";max-age=5";
+            setInterval(() => {
+                document.cookie = "satoken=" + saToken[1] + ";domain=." + ROOT_DOMAIN + ";max-age=5";
+            }, 5000);
             break;
         }
     }
